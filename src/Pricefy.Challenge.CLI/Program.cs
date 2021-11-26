be1c51f32502f2a22d43432bcb8ed7b3fb047111
Allan Egidio <allan.egidio@outlook.com>
2021-11-25 21:11:01 -0300

    Creating Base project

---
+++ src/Pricefy.Challenge.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pricefy.Challenge.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
