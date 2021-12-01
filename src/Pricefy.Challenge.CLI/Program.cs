a481cb12f591735d8237440a7f334a018ff5a744
Allan Egidio <allan.egidio@outlook.com>
2021-11-30 22:07:24 -0300

    add write of tsv file

--- src/Pricefy.Challenge.CLI/Program.cs
+++ src/Pricefy.Challenge.CLI/Program.cs
@@ -12,23 +12,32 @@ namespace Pricefy.Challenge.CLI
 {
     public class Title
     {
+        [Name("tconst")]
         public string Id { get; set; }
 
+        [Name("titleType")]
         public string Type { get; set; }
 
+        [Name("primaryTitle")]
         public string PrimaryTitle { get; set; }
 
+        [Name("originalTitle")]
         public string OriginalTitle { get; set; }
 
+        [Name("isAdult")]
         public bool IsAdult { get; set; }
 
+        [Name("startYear")]
         public string StartYear { get; set; }
 
+        [Name("endYear")]
         public string EndYear { get; set; }
 
+        [Name("runtimeMinutes")]
         public string RuntimeMinutes { get; set; }
 
-        public List<string> Genres { get; set; }
+        [Name("genres")]
+        public string Genres { get; set; }
     }
 
     class Program
@@ -53,27 +62,60 @@ namespace Pricefy.Challenge.CLI
                 Mode = CsvMode.NoEscape
             };
 
-            using (var reader = new StreamReader(@"C:/titles/data.tsv"))
+
+            using (var reader = new StreamReader(@"D:/titles/data.tsv"))
             using (var csv = new CsvReader(reader, config))
             {
+                int id = 1;
+                int totalRowsFile = 1000;
                 csv.Read();
                 csv.ReadHeader();
 
-                while (csv.Read())
+                var titles = csv.GetRecords<Title>().ToList();
+
+                for (var row = 0; row <= titles.Count / totalRowsFile; row++, id++)
                 {
-                    listTitles.Add(new Title()
+                    var fileRows = titles.Skip(row * totalRowsFile)
+                                        .Take(totalRowsFile);
+
+
+                    var outputPath = Path.Combine(@"D:/titles/chunk", @$"titles.file.{id}.{DateTime.Now.ToString("yyyy-MM-dd")}.tsv");
+
+                    using (var writer = new StreamWriter(outputPath, false, System.Text.Encoding.UTF8))
                     {
-                        Id = csv.GetField<string>("tconst"),
-                        Type = csv.GetField<string>("titleType"),
-                        PrimaryTitle = csv.GetField<string>("primaryTitle"),
-                        OriginalTitle = csv.GetField<string>("originalTitle"),
-                        IsAdult = csv.GetField<bool>("isAdult"),
-                        StartYear = csv.GetField<string>("startYear"),
-                        EndYear = csv.GetField<string>("endYear"),
-                        RuntimeMinutes = csv.GetField<string>("runtimeMinutes"),
-                        Genres = csv.GetField<string>("genres").Split(",").ToList()
-                    });
+                        using (var csvFile = new CsvWriter(writer, config))
+                        {
+                            csvFile.WriteRecords(fileRows);
+                        }
+                    }
                 }
+
+                // while (csv.Read())
+                // {
+                //     chunk++;
+
+                //     if (chunk == 1000)
+                //     {
+                //         chunk = 0;
+                //         id++;
+                //         // Escreve novo arquivo e envia para API
+                //         var outputPath = Path.Combine(@"D:/titles/chunk", $"titles.file.{id}.{DateTime.Now.ToString("yyyy-MM-dd")}");
+
+                //     }
+
+                //     listTitles.Add(new Title()
+                //     {
+                //         Id = csv.GetField<string>("tconst"),
+                //         Type = csv.GetField<string>("titleType"),
+                //         PrimaryTitle = csv.GetField<string>("primaryTitle"),
+                //         OriginalTitle = csv.GetField<string>("originalTitle"),
+                //         IsAdult = csv.GetField<bool>("isAdult"),
+                //         StartYear = csv.GetField<string>("startYear"),
+                //         EndYear = csv.GetField<string>("endYear"),
+                //         RuntimeMinutes = csv.GetField<string>("runtimeMinutes"),
+                //         Genres = csv.GetField<string>("genres").Split(",").ToList()
+                //     });
+                // }
             }
 
             stopwatch.Stop();
