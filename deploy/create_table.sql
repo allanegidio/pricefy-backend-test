46fc16d8120d895feb507c00d1398d5a0011bdd8
Allan Egidio <allan.egidio@outlook.com>
2021-12-01 16:43:41 -0300

    add scripts sql

---
+++ deploy/create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Titles
+(
+    Id VARCHAR(255) PRIMARY KEY,
+    Type VARCHAR(255),
+    PrimaryTitle VARCHAR(255),
+    OriginalTitle VARCHAR(255),
+    IsAdult BIT,
+    StartYear VARCHAR(255),
+    EndYear VARCHAR(255),
+    RuntimeMinutes VARCHAR(255),
+    Genres VARCHAR(255),
+);
